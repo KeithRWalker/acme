8af2545effe898a54eaefa3e045798c48c8c5a94
Keith Walker <Keithrwalker418@gmail.com>
2019-05-18 21:13:24 -0500

    WIP

--- src/javascripts/components/cards/cards.js
+++ src/javascripts/components/cards/cards.js
@@ -1,10 +1,10 @@
-// import categoriesData from '../../helpers/data/categoriesData';
+import categoriesData from '../../helpers/data/categoriesData';
 
-// import typesData from '../../helpers/data/typesData';
+import typesData from '../../helpers/data/typesData';
 
-// import productData from '../../helpers/data/productData';
+import productData from '../../helpers/data/productData';
 
-// import util from '../../helpers/util';
+import util from '../../helpers/util';
 
 import typesData from '../../helpers/data/typesData';
 
@@ -13,65 +13,3 @@ const loadProducts = () => {
 };
 
 export default { loadProducts };
-
-// let prodAr = [];
-
-// const cardDisplay = (ar) => {
-//   let cardSt = '';
-//   ar.forEach((card) => {
-//     cardSt += '<div class="card">';
-//     cardSt += `<h1>${card.name}</ar>`;
-//     cardSt += `<h1>${card.type}</h1>`;
-//     cardSt += '</div>';
-//   });
-//   util.printToDom('app', cardSt);
-// };
-
-// const initCategories = () => {
-//   categoriesData.loadCategories()
-//     .then((resp) => {
-//       const catsA = resp.data.categories;
-//       cardDisplay(catsA);
-//     })
-//     .catch(err => console.error('fuck', err));
-// };
-
-// const initTypes = () => {
-//   typesData.loadTypes()
-//     .then((resp) => {
-//       const typesA = resp.data.types;
-//       // const meleeType = typesA.filter(type => type.id === 0);
-//       cardDisplay(typesA);
-//     })
-//     .catch(err => console.error('types fuck', err));
-// };
-
-// const initProducts = () => {
-//   productData.loadProducts()
-//     .then((resp) => {
-//       const allProducts = resp;
-//       allProducts.forEach((item) => {
-//         const newAr = Object.values(item);
-//         console.error(newAr);
-//         cardDisplay(newAr);
-//         // console.error(newAr);
-//       });
-//       // console.error(allProducts);
-//     })
-//     .catch(err => console.error('products FUCK', err));
-// };
-
-// const filterProducts = () => {
-//   typesData.loadTypes()
-//     .then((resp) => {
-//       for (let i = 0; i <= resp.length; i += 1) {
-//         const typeId = resp[i].id;
-//         productData.loadProducts(typeId)
-//           .then((products) => {
-//             console.error(products);
-//             cardDisplay(products);
-//           });
-//       }
-//     })
-//     .catch(err => console.error('error', err));
-// };
--- src/javascripts/helpers/data/typesData.js
+++ src/javascripts/helpers/data/typesData.js
@@ -1,31 +1,5 @@
 import axios from 'axios';
 
-import categoriesData from './categoriesData';
-
-const catType = [];
-
-const loadCategories = () => {
-  categoriesData.loadCategories()
-    .then((resp) => {
-      const catsA = resp.data.categories;
-      console.error('loadCategories => catsA', catsA);
-      catType.push(catsA);
-      return catsA;
-    })
-    .catch(err => console.error('fuck', err));
-};
-
-const loadTypes = () => {
-  axios.get('../db/types.json')
-    .then((resp) => {
-      const typeId = resp.data;
-      console.error('loadTypes => typeId ', typeId);
-      catType.push(typeId);
-      loadCategories();
-      console.error(catType);
-    })
-    .catch();
-};
-
+const loadTypes = () => axios.get('../db/types.json');
 
 export default { loadTypes };
