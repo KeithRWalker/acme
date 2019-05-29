34e3d4d937f3dddaed849941cfa9005f148a518b
Keith Walker <Keithrwalker418@gmail.com>
2019-05-28 21:37:23 -0500

    cards displaying finally!

--- src/javascripts/components/products/products.js
+++ src/javascripts/components/products/products.js
@@ -6,9 +6,9 @@ import categoryData from '../../helpers/data/categoriesData';
 
 import util from '../../helpers/util';
 
-const typesArray = [];
+let typesArray = [];
 
-const categoriesArray = [];
+let categoriesArray = [];
 
 const productArray = [];
 
@@ -18,9 +18,31 @@ const cardBuild = (array) => {
     cardSt += `<div id="${product.type}" class="card col-2 ${product.type}">`;
     cardSt += `<h2>${product.name}</h2>`;
     cardSt += `<p>${product.description}</p>`;
+    cardSt += `<p>${product.categoryName}</p>`;
+    cardSt += `<p>${product.typeName}</p>`;
     cardSt += '</div>';
   });
   util.printToDom('app', cardSt);
+};
+
+
+const smashFunction = () => {
+  const products = [];
+  productArray.forEach((p) => {
+    const product = p;
+    typesArray.forEach((type) => {
+      if (product.type === type.id) {
+        product.typeName = type.name;
+        categoriesArray.forEach((cat) => {
+          if (type.category === cat.id) {
+            product.categoryName = cat.name;
+          }
+        });
+      }
+    });
+    products.push(product);
+  });
+  cardBuild(products);
 };
 
 const initProducts = () => {
@@ -30,8 +52,7 @@ const initProducts = () => {
       product.forEach((item) => {
         productArray.push(item);
       });
-      cardBuild(productArray);
-      console.error('Products Array', productArray);
+      smashFunction();
     });
 };
 
@@ -39,10 +60,11 @@ const initTypes = () => {
   typeData.loadTypes()
     .then((resp) => {
       const { types } = resp.data;
-      types.forEach((type) => {
-        typesArray.push(type);
-      });
-      console.error('Types Array', typesArray);
+      // types.forEach((type) => {
+      //   typesArray.push(type);
+      // });
+      typesArray = types;
+      initProducts();
     })
     .catch(err => console.error(err));
 };
@@ -51,17 +73,17 @@ const initCategories = () => {
   categoryData.loadCategories()
     .then((resp) => {
       const { categories } = resp.data;
-      categories.forEach((category) => {
-        categoriesArray.push(category);
-      });
-      console.error('Categories Array', categoriesArray);
+      // categories.forEach((category) => {
+      //   categoriesArray.push(category);
+      // });
+      // console.error('Categories Array', categoriesArray);
+      categoriesArray = categories;
+      initTypes();
     })
     .catch(err => console.error(err, 'error from loadCategories'));
 };
 
 const initAll = () => {
-  initProducts();
-  initTypes();
   initCategories();
 };
 
